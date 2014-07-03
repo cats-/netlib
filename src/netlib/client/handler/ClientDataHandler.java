d35f63c45b49f0adc8672164d94152096c8ca269
cats- <joshua.maione@hotmail.com>
2014-07-02 21:50:42 -0400

    Generic client type

--- src/netlib/client/handler/ClientDataHandler.java
+++ src/netlib/client/handler/ClientDataHandler.java
@@ -4,8 +4,8 @@ import netlib.client.Client;
 import netlib.core.data.Data;
 import netlib.core.data.handler.DataHandler;
 
-public interface ClientDataHandler extends DataHandler{
+public interface ClientDataHandler<T extends Client> extends DataHandler{
 
-    public void handle(final Client client, final Data data);
+    public void handle(final T client, final Data data);
 
 }
