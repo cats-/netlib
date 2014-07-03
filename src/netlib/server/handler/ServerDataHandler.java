c6b8a1ad356d0144a414a858e392642aa9c1d614
cats- <joshua.maione@hotmail.com>
2014-07-02 21:50:16 -0400

    Generic server type

--- src/netlib/server/handler/ServerDataHandler.java
+++ src/netlib/server/handler/ServerDataHandler.java
@@ -5,7 +5,7 @@ import netlib.core.data.handler.DataHandler;
 import netlib.server.Server;
 import netlib.server.connection.Connection;
 
-public interface ServerDataHandler extends DataHandler{
+public interface ServerDataHandler<T extends Server> extends DataHandler{
 
-    public void handle(final Server server, final Connection connection, final Data data);
+    public void handle(final T server, final Connection connection, final Data data);
 }
