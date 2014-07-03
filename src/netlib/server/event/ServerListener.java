950b505cc2a0c875e39b0e658e6b2fd061fa9785
cats- <joshua.maione@hotmail.com>
2014-07-02 22:13:17 -0400

    Added generic type argument

--- src/netlib/server/event/ServerListener.java
+++ src/netlib/server/event/ServerListener.java
@@ -3,13 +3,13 @@ package netlib.server.event;
 import netlib.server.Server;
 import netlib.server.connection.Connection;
 
-public interface ServerListener {
+public interface ServerListener<T extends Server> {
 
-    public void onStart(final Server server);
+    public void onStart(final T server);
 
-    public void onFinish(final Server server);
+    public void onFinish(final T server);
 
-    public void onJoin(final Server server, final Connection con);
+    public void onJoin(final T server, final Connection con);
 
-    public void onLeave(final Server server, final Connection con);
+    public void onLeave(final T server, final Connection con);
 }
