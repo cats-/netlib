e512804568b301547fd7cb84d54b7d89c26b8478
cats- <joshua.maione@hotmail.com>
2014-07-02 22:02:06 -0400

    Converted interface to abstract class

--- src/netlib/client/handler/ClientDataHandler.java
+++ src/netlib/client/handler/ClientDataHandler.java
@@ -4,8 +4,12 @@ import netlib.client.Client;
 import netlib.core.data.Data;
 import netlib.core.data.handler.DataHandler;
 
-public interface ClientDataHandler<T extends Client> extends DataHandler{
+public abstract class ClientDataHandler<T extends Client> implements DataHandler{
 
-    public void handle(final T client, final Data data);
+    public abstract void handle(final T client, final Data data) throws Exception;
+
+    public void handleException(final T client, final Data data, final Exception ex){
+        ex.printStackTrace();
+    }
 
 }
--- src/netlib/client/handler/DefaultClientDataHandler.java
+++ src/netlib/client/handler/DefaultClientDataHandler.java
@@ -3,7 +3,7 @@ package netlib.client.handler;
 import netlib.client.Client;
 import netlib.core.data.Data;
 
-public class DefaultClientDataHandler implements ClientDataHandler{
+public class DefaultClientDataHandler extends ClientDataHandler{
 
     public void handle(final Client client, final Data data){
         System.out.printf("[%s] %s", client, data);
